5acdabd6f8c467c24dab59c869c30ffefb92dde3
Rurik02 <manu.dag20@gmail.com>
2024-05-29 01:30:42 +0200

    Gui v0.1

---
+++ gui/root.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import customtkinter as ctk
+from button_manager import Btn_manager
+from label_manager import Lbl_manager
+from window_manager import Wnd_manager
+
+
+
+class Root:
+    """
+        Custom tkinter main window
+    """
+    def __init__(self):
+        self.__root = ctk.CTk()
+
+        wnd_man = Wnd_manager(self.__root)
+        btn_man = Btn_manager(self.__root, wnd_man)
+        lbl_man = Lbl_manager(self.__root)
+
+        btn_man.main_menu()
+
+        self.__root.mainloop()
+
+
+
+
+
+
+
---
+++ gui/window_manager.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import customtkinter as ctk
+
+
+class Wnd_manager:
+    """
+        This class manage all the graphics options of
+        the window.
+    """
+    def __init__(self, root):
+        self.__dark_theme = True
+        self.__set_window(root)
+
+
+    def __set_window(self, root):
+        """
+            Set window values and options.
+        """
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
+
+        root.geometry("900x550")
+        root.maxsize(1100, 650)
+        root.minsize(700, 450)
+        root.iconbitmap("sudoku.ico")
+        root.title("SolveDoku!")
+
+
+    def switch_theme(self):
+        """
+            Switch theme from dark to light and vice versa.
+        """
+        if not self.__dark_theme :
+            theme = "dark"
+            theme_color = "dark_blue"
+            self.__dark_theme = True
+        else:
+            theme = "light"
+            theme_color = "blue"
+            self.__dark_theme = False
+        
+        ctk.set_appearance_mode(theme)
+        ctk.set_default_color_theme(theme_color)
