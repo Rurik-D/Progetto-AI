6397b2e8160acf3947a3365597d986fc822498e3
Rurik02 <manu.dag20@gmail.com>
2024-05-29 18:33:09 +0200

    _

--- gui/root.py
+++ gui/root.py
@@ -1,47 +1,30 @@
 from tkinter import *
 import customtkinter as ctk
-
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
-
-root = ctk.CTk()
-root.geometry("700x500")
-
-switch = False
-
-def btn_function():
-    global switch
-
-    if not switch:
-        lbl.configure(text = "Tronci >>>>>> Mancini")
-        switch = True
-    else:
-        lbl.configure(text = "")
-        switch = False
+from button_manager import Btn_manager
+from label_manager import Lbl_manager
+from window_manager import Wnd_manager
 
 
-btn = ctk.CTkButton(root, 
-                    text="This is not a button", 
-                    command=btn_function,
-                    height=100,
-                    width=200,
-                    font=("Helvetica", 20),
-                    text_color="black",
-                    fg_color="#1ca9da",
-                    hover_color="#1286b9",
-                    corner_radius=50,
-                    bg_color="#000000",
-                    border_width=5,
-                    border_color="white",
-                    border_spacing=50,
-                    state="normal") # "disabled"
-                    
 
-btn.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+class Root:
+    """
+        Custom tkinter main window
+    """
+    def __init__(self):
+        self.__root = ctk.CTk()
 
-lbl = ctk.CTkLabel(root, text="")
-lbl.place(relx=0.5, rely=0.7, anchor=ctk.CENTER)
+        wnd_man = Wnd_manager(self.__root)
+        btn_man = Btn_manager(self.__root, wnd_man)
+        lbl_man = Lbl_manager(self.__root)
+
+        lbl_man.show_title()
+        btn_man.main_menu()
+
+        self.__root.mainloop()
 
 
-root.mainloop()
+
+
+
+
 
